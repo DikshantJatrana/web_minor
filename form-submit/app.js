0c063e0ee83f638d82d90113ba43acaeab2a75ab
DikshantJatrana <dikshantjatrana02@gmail.com>
2024-01-03 11:11:51 +0530

    made form HTML and CSS design

---
+++ form-submit/app.js
@@ -0,0 +1,4 @@
+const next = document.querySelector("#next");
+const card1 = document.querySelector("#card1");
+const card2 =  document.querySelector("#card2");
+const card3 = document.querySelector("#card3");
